9cd36cda9729b5124b606278987d527f0269217d
jrpinzon88 <jrpinzon88@gmail.com>
2023-01-10 21:09:25 -0500

    Se agrega tema de promesas

--- challenge.js
+++ challenge.js
@@ -5,7 +5,8 @@ const API = "https://api.escuelajs.co/api/v1";
 function fetchData(urlApi, callback) {
     //inicializar un objeto de tipo XMLHttpRequest
     let xhttp = new XMLHttpRequest();
-    //El metodo .open realiza la petición de apertura de comunicación, el metodo puede ser 'GET' o 'POST', luego se envia la URL, si es asincrono (true o false), usuario y contraseña. En esta caso solo se utiliza el metodo, la url y async
+    //El metodo .open realiza la petición de apertura de comunicación, el metodo puede ser 'GET' o 'POST', luego se envia la URL, 
+    //si es asincrono (true o false), usuario y contraseña. En esta caso solo se utiliza el metodo, la url y async
     xhttp.open('GET', urlApi, true);
     //en este metodo Almacena el nombre de la función que se ejecutará cuando el objeto XMLHttpRequest cambie de estado
     xhttp.onreadystatechange = function (event) {
@@ -18,12 +19,15 @@ function fetchData(urlApi, callback) {
         if (xhttp.readyState === 4) {
             //si la respuesta de la API es exitosa (200 Ok)
             if (xhttp.status === 200) {
-                //se ejecuta el callback recibiendo como argumentos un objeto, como la respuesta de la API es un texto plano, el metodo JSON.parse tranformará este texto en un objeto.
-                //El atributo devuelve un DOMString que contiene la  respuesta a la consulta como un texto o null si la consulta no tuvo exito o aun no ha sido completada.
+                //se ejecuta el callback recibiendo como argumentos un objeto, como la respuesta de la API es un texto plano, 
+                //el metodo JSON.parse tranformará este texto en un objeto.
+                //El atributo devuelve un DOMString que contiene la  respuesta a la consulta como un texto o null 
+                //si la consulta no tuvo exito o aun no ha sido completada.
                 callback(null, JSON.parse(xhttp.responseText));
                 //si la respuesta de la API no es exitosa se captura el error
             } else {
-                //se inicializa un objeto de tipo Error donde se le envian como argumentos un mensaje de error y la URL de la API para conocer en dónde se produjo el error
+                //se inicializa un objeto de tipo Error donde se le envian como argumentos un mensaje de error y 
+                //la URL de la API para conocer en dónde se produjo el error
                 const error = new Error("Error" + urlApi);
                 //se ejecuta el callback recibiendo como argumentos el error y null debido a que no se pudo obtener el objeto
                 return callback(error, null);
@@ -34,11 +38,14 @@ function fetchData(urlApi, callback) {
   xhttp.send();
 }
 
-//se invoca el metodo fetchData() pasandole como argumentos la varible API concatenada con la cadena 'products' para acceder a la URL de la API deseada, y una función anónima que recibe 2 parámetros (un objeto de error y un arreglo que almacena todos los objetos traidos por la API).
+//se invoca el metodo fetchData() pasandole como argumentos la varible API concatenada con la cadena 'products' 
+//para acceder a la URL de la API deseada, y una función anónima que recibe 2 parámetros 
+//(un objeto de error y un arreglo que almacena todos los objetos traidos por la API).
 fetchData(`${API}/products`, function (error1, data1) {
     //se valida si existe un error, en caso de que exista se detiene el proceso y se imprime el error
     if (error1) return console.error(error1);
-    //se invoca nuevamente la función fetchData con el fin de acceder a un objeto puntual del arreglo data1, se envia como parámetros la url de la API apuntando al atributo del primer objeto de arreglo data1 y nuevamente una función anónima.
+    //se invoca nuevamente la función fetchData con el fin de acceder a un objeto puntual del arreglo data1, 
+    //se envia como parámetros la url de la API apuntando al atributo del primer objeto de arreglo data1 y nuevamente una función anónima.
     fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
         //si en este punto se identifica un error se imprime en consola y se detiene el proceso
         if (error2) return console.error(error2);
